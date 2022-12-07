4251653446babdbe33ff562854d848e795e356fa
unknown <miguel.semedo.2@outlook.com>
2022-12-07 19:36:20 +0000

    fetch user data from server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'yoyo', age: 20 });
+const user = new User({ id: 1 });
+user.fetch();
 
-user.on('click', () => {
-	console.log('click 1');
-});
-user.on('change', () => {
-	console.log('change 2');
-});
-user.on('save', () => {
-	console.log('saved');
-});
-
-user.trigger('click');
-user.trigger('change');
-user.trigger('save');
-
-console.log(user.events);
+setTimeout(() => {
+	console.log(user);
+}, 400);
