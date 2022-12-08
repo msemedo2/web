b5c3f5d526c3f3386e5167433357f7fc69f0a709
unknown <miguel.semedo.2@outlook.com>
2022-12-08 08:46:47 +0000

    refactor events functionality: Eventing class

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'carl', age: 0 });
 
 user.save();
+user.events.on('change', () => {
+	console.log('change');
+});
+user.events.trigger('change');
