986720186ed315a1766d4ab93c2f041a9e52c7a4
unknown <miguel.semedo.2@outlook.com>
2022-12-08 10:06:43 +0000

    create Sync class

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'carl', age: 0 });
 
-user.save();
-user.events.on('change', () => {
-	console.log('change');
-});
-user.events.trigger('change');
+// user.save();
+// user.events.on('change', () => {
+// 	console.log('change');
+// });
+// user.events.trigger('change');
